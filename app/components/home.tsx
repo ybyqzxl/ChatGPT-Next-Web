b00e6138feaf5a8af7563fcfab66318f6de95847
zhaoxueli <zhaoxueli@kuaishou.com>
2023-04-14 15:08:13 +0800

    修改提示信息

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>Panda's ChatGPT</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            Panda's AI assistant.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
